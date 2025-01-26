681c642ef26e0bb51fe064686092999d102b354e
th3a1ph4d0g <th3a1ph4d0g@gmail.com>
2025-01-26 14:48:37 -0600

    [Logalyzer] - Update Polish question
    This question should actually only track if POL joined the DA

--- webdriver_form_filler.py
+++ webdriver_form_filler.py
@@ -95,7 +95,7 @@ def extract_data_for_webdriver_script(log_data: dict) -> list:
             "Did the Netherlands go socialist?": "No",
             "Who controls most of the Indian subcontinent?": "Split between starting nations",
             "Which faction did Greece join?": "None",
-            "Which faction did Poland join?": "Reichspakt",
+            "Did Poland join the Donau-Adriabund?": "No",
             "Who won the Spanish Civil War?": "Nobody",
             "When did the Spanish Civil War end?": "Did not end",
             "Who won the Indochinese War?": "Did not happen",
@@ -429,8 +429,8 @@ def extract_data_for_webdriver_script(log_data: dict) -> list:
             elif m := re.match(r'GRE ALLIANCE - (.*)', i):
                 game_results["Which faction did Greece join?"] = m.group(1)
 
-            elif m := re.match(r'POL ALLIANCE - (.*)', i):
-                game_results["Which faction did Poland join?"] = m.group(1)
+            elif m := re.match(r'POL ALLIANCE - Donau-Adriabund', i):
+                game_results["Did Poland join the Donau-Adriabund?"] = "Yes"
 
             elif m := re.match(r'FNG-QIE WAR START', i):
                 game_results["When did the Zhifeng War start?"] = generate_text_field_output(m=m, log_data=log_data)
